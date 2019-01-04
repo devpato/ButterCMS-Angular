7901f8dbe0c226a678d1e2623004bbba618ab13b
pevargasg <wearetamo@gmail.com>
2019-01-04 01:52:59 -0800

    app and home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BUTTER_SERVICE } from "../shared/butterCMS.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  page: any;
+  constructor() {}
 
   ngOnInit() {
+    this.fetchPage();
   }
 
+  fetchPage() {
+    BUTTER_SERVICE.page
+      .retrieve("*", "homepage")
+      .then(res => {
+        this.page = res.data.data;
+        console.log(this.page);
+      })
+      .catch(res => {
+        console.log(res);
+      });
+  }
 }
