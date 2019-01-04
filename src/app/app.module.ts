fa38edc8a6502a03ab3d55165d4b294d07fca40f
pevargasg <wearetamo@gmail.com>
2019-01-03 18:50:39 -0800

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './postlist/postlist.component';
+import { BlogPostDetailsComponent } from './blog-post-details/blog-post-details.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerListingComponent } from './customer-listing/customer-listing.component';
+import { FaqComponent } from './faq/faq.component';
+import { FeedComponent } from './feed/feed.component';
+import { HomeComponent } from './home/home.component';
+import { TopavComponent } from './topav/topav.component';
+import { HelloYouComponent } from './hello-you/hello-you.component';
+import { BlogPostListingComponent } from './blog-post-listing/blog-post-listing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostlistComponent,
+    BlogPostDetailsComponent,
+    CustomerDetailsComponent,
+    CustomerListingComponent,
+    FaqComponent,
+    FeedComponent,
+    HomeComponent,
+    TopavComponent,
+    HelloYouComponent,
+    BlogPostListingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/hello-you/hello-you.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-you',
+  template: `
+    <p>
+      hello-you works!
+    </p>
+  `,
+  styles: []
+})
+export class HelloYouComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
