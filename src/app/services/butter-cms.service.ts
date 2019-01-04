4a8674b77d85f1a71e97733b4897943ece52c3fa
pevargasg <wearetamo@gmail.com>
2019-01-03 18:49:47 -0800

    butter cms service

--- src/app/services/butter-cms.service.ts
+++ src/app/services/butter-cms.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from "@angular/core";
-import * as Butter from "buttercms";
 
 @Injectable({
   providedIn: "root"
 })
 export class ButterCMSService {
-  butterService = Butter("2f59c1ea2f1e098c4d9f17556cb446dd51c43d4d");
   constructor() {}
 }
---
+++ src/app/shared/butterCMS.service.ts
@@ -0,0 +1,5 @@
+import * as Butter from "buttercms";
+
+export const BUTTER_SERVICE = Butter(
+  "2f59c1ea2f1e098c4d9f17556cb446dd51c43d4d"
+);
