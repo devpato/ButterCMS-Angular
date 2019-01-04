557b165669decceaf9d0e376960634098ef50b3b
pevargasg <wearetamo@gmail.com>
2019-01-03 19:32:45 -0800

    app cleanup and example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { PostlistComponent } from './postlist/postlist.component';
-import { BlogPostDetailsComponent } from './blog-post-details/blog-post-details.component';
-import { CustomerDetailsComponent } from './customer-details/customer-details.component';
-import { CustomerListingComponent } from './customer-listing/customer-listing.component';
-import { FaqComponent } from './faq/faq.component';
-import { FeedComponent } from './feed/feed.component';
-import { HomeComponent } from './home/home.component';
-import { TopavComponent } from './topav/topav.component';
-import { HelloYouComponent } from './hello-you/hello-you.component';
-import { BlogPostListingComponent } from './blog-post-listing/blog-post-listing.component';
+import { AppComponent } from "./app.component";
+import { BlogPostDetailsComponent } from "./blog-post-details/blog-post-details.component";
+import { CustomerDetailsComponent } from "./customer-details/customer-details.component";
+import { CustomerListingComponent } from "./customer-listing/customer-listing.component";
+import { FaqComponent } from "./faq/faq.component";
+import { FeedComponent } from "./feed/feed.component";
+import { HomeComponent } from "./home/home.component";
+import { TopavComponent } from "./topav/topav.component";
+import { HelloYouComponent } from "./hello-you/hello-you.component";
+import { BlogPostListingComponent } from "./blog-post-listing/blog-post-listing.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostlistComponent,
     BlogPostDetailsComponent,
     CustomerDetailsComponent,
     CustomerListingComponent,
@@ -27,10 +25,8 @@ import { BlogPostListingComponent } from './blog-post-listing/blog-post-listing.
     HelloYouComponent,
     BlogPostListingComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/hello-you/hello-you.component.ts
+++ src/app/hello-you/hello-you.component.ts
@@ -1,19 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { BUTTER_SERVICE } from "../shared/butterCMS.service";
 
 @Component({
-  selector: 'app-hello-you',
+  selector: "app-hello-you",
   template: `
-    <p>
-      hello-you works!
-    </p>
+    <h1>Hello-You</h1>
+    <p>Headline: {{ headline?.headline }}</p>
+    <p>Sample Post: {{ posts ? posts.data[0]?.url : "no posts" }}</p>
   `,
   styles: []
 })
 export class HelloYouComponent implements OnInit {
+  posts: any;
+  headline: any;
+  page: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.fetchPosts();
+    this.fetchHeadline();
   }
 
+  private fetchPosts() {
+    BUTTER_SERVICE.post
+      .list({
+        page: 1,
+        page_size: 10
+      })
+      .then(res => {
+        console.log("Content from ButterCMS");
+        console.log(res);
+        this.posts = res.data;
+      });
+  }
+
+  private fetchHeadline() {
+    BUTTER_SERVICE.page
+      .retrieve("*", "homepage")
+      .then(res => {
+        this.headline = res.data.data.fields;
+        console.log(this.headline.headline);
+      })
+      .catch(res => {
+        console.log(res);
+      });
+  }
+
+  //contentype retrival
+  // butterService.content.retrieve(['homepage_headline'])
+  // .then((res) => {
+  //     console.log('Headline from ButterCMS');
+  //     console.log(res);
+  //     this.headlines = res.data;
+  // });
 }
