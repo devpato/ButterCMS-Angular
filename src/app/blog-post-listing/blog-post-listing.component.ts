dbd40d445824f51deb8ccd0dfd3abfbe6db23678
pevargasg <wearetamo@gmail.com>
2019-01-03 18:48:16 -0800

    blog post listing

---
+++ src/app/blog-post-listing/blog-post-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-listing',
+  templateUrl: './blog-post-listing.component.html',
+  styleUrls: ['./blog-post-listing.component.css']
+})
+export class BlogPostListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
