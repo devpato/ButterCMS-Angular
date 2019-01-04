36fb976f34db4ed9295cb131612df6b193f3419c
pevargasg <wearetamo@gmail.com>
2019-01-03 18:50:09 -0800

    topav component

---
+++ src/app/topav/topav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topav',
+  templateUrl: './topav.component.html',
+  styleUrls: ['./topav.component.css']
+})
+export class TopavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
