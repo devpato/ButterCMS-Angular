b54062cd8122f1ed4242b7912e86ecf068043ffc
pevargasg <wearetamo@gmail.com>
2019-01-04 01:51:04 -0800

    customer details

--- src/app/customer-details/customer-details.component.ts
+++ src/app/customer-details/customer-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { map, take } from 'rxjs/operators';
+import { BUTTER_SERVICE } from '../shared/butterCMS.service';
 
 @Component({
   selector: 'app-customer-details',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected route: ActivatedRoute) { }
+
+  protected slug$: Observable<string>;
+  public page: any;
 
   ngOnInit() {
+    this.slug$ = this.route.paramMap
+      .pipe(
+        map(params => (params.get('slug')))
+      );
+
+    this.slug$.pipe(
+      take(1))
+      .subscribe(slug => {
+        BUTTER_SERVICE.page.retrieve('case_of_study', slug)
+          .then((res) => {
+            this.page = res.data.data;
+          }).catch((res) => {
+            console.log(res);
+          });
+      });
   }
 
 }
--- src/app/customer-listing/customer-listing.component.ts
+++ src/app/customer-listing/customer-listing.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { BUTTER_SERVICE } from "../shared/butterCMS.service";
+import { RouterModule, Routes } from "@angular/router";
 @Component({
-  selector: 'app-customer-listing',
-  templateUrl: './customer-listing.component.html',
-  styleUrls: ['./customer-listing.component.css']
+  selector: "app-customer-listing",
+  templateUrl: "./customer-listing.component.html",
+  styleUrls: ["./customer-listing.component.css"]
 })
 export class CustomerListingComponent implements OnInit {
-
-  constructor() { }
+  pages: any[];
+  constructor() {}
 
   ngOnInit() {
+    this.fetchPages();
   }
 
+  fetchPages(): void {
+    BUTTER_SERVICE.page.list("case_of_study").then(res => {
+      this.pages = res.data.data;
+      console.log("pages");
+      console.log(this.pages);
+    });
+  }
 }
